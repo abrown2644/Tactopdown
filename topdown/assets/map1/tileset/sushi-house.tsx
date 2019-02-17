e52a0e213d233de30de244e0cc852353310fad8d
Andrew Brown <32423987+abrown2644@users.noreply.github.com>
2019-02-16 22:25:38 -0500

    Add files via upload

---
+++ topdown/assets/map1/tileset/sushi-house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="sushi-house" tilewidth="16" tileheight="16" tilecount="77400" columns="430">
+ <image source="../pngs/sushi-house.png" width="6880" height="2880"/>
+</tileset>
---
+++ topdown/assets/testmap/tilesets/test-floor.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="test-floor" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../png/test-floor.png" width="128" height="128"/>
+ <tile id="56">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="17" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
